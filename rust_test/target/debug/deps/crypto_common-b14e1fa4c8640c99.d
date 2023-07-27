b24371b023a9363868962341884885e1c611412d
Justin <34551787+HJK-X@users.noreply.github.com>
2023-07-26 20:35:05 -0700

    testing builds

---
+++ rust_test/target/debug/deps/crypto_common-b14e1fa4c8640c99.d
@@ -0,0 +1,7 @@
+C:\Users\justi\Desktop\Projects\Test\rust_test\target\debug\deps\crypto_common-b14e1fa4c8640c99.rmeta: C:\Users\justi\.cargo\registry\src\index.crates.io-6f17d22bba15001f\crypto-common-0.1.6\src\lib.rs
+
+C:\Users\justi\Desktop\Projects\Test\rust_test\target\debug\deps\libcrypto_common-b14e1fa4c8640c99.rlib: C:\Users\justi\.cargo\registry\src\index.crates.io-6f17d22bba15001f\crypto-common-0.1.6\src\lib.rs
+
+C:\Users\justi\Desktop\Projects\Test\rust_test\target\debug\deps\crypto_common-b14e1fa4c8640c99.d: C:\Users\justi\.cargo\registry\src\index.crates.io-6f17d22bba15001f\crypto-common-0.1.6\src\lib.rs
+
+C:\Users\justi\.cargo\registry\src\index.crates.io-6f17d22bba15001f\crypto-common-0.1.6\src\lib.rs:

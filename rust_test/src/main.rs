7ddc129c7252b822cdddb4a14302934a9e433cad
Justin <34551787+HJK-X@users.noreply.github.com>
2023-07-27 21:02:04 -0700

    filling out more of the cli

--- rust_test/src/main.rs
+++ rust_test/src/main.rs
@@ -1,4 +1,4 @@
-use clap::{Parser, Subcommand};
+use clap::{Args, Parser, Subcommand};
 
 #[derive(Parser)]
 #[command(author, version, about, long_about = None)]
@@ -11,9 +11,35 @@ struct Cli {
 
 #[derive(Subcommand)]
 enum Commands {
-    /// Adds fiels to app
-    Add {name: Option<String>},
+    /// Add a password
+    Add(DefaultArgs),
     
+    /// Get a password
+    Get(DefaultArgs),
+
+    /// List all stored services
+    List(DefaultArgs),
+
+    /// Update a password
+    Update(DefaultArgs),
+
+    /// Delete a password
+    Delete(DefaultArgs),
+
+    /// View password history
+    History(DefaultArgs),
+
+    
+}
+
+#[derive(Args)]
+struct DefaultArgs {
+    /// Website URL/Name
+    website: Option<String>,
+    /// Username/Email
+    username: Option<String>,
+    /// Password/Secure Note
+    password: Option<String>,
 }
 
 

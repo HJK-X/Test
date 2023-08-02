1dd612d8e63f88f593b48b36de2018ea5968e3bf
Justin <34551787+HJK-X@users.noreply.github.com>
2023-08-02 16:49:58 -0700

    gonna do argon 2

---
+++ rust_test/target/debug/deps/argon2-c87b60e5b13a68e7.d
@@ -0,0 +1,12 @@
+C:\Users\justi\Desktop\Projects\Test\rust_test\target\debug\deps\argon2-c87b60e5b13a68e7.rmeta: C:\Users\justi\.cargo\registry\src\index.crates.io-6f17d22bba15001f\argon2-0.5.1\src\lib.rs C:\Users\justi\.cargo\registry\src\index.crates.io-6f17d22bba15001f\argon2-0.5.1\src\algorithm.rs C:\Users\justi\.cargo\registry\src\index.crates.io-6f17d22bba15001f\argon2-0.5.1\src\blake2b_long.rs C:\Users\justi\.cargo\registry\src\index.crates.io-6f17d22bba15001f\argon2-0.5.1\src\block.rs C:\Users\justi\.cargo\registry\src\index.crates.io-6f17d22bba15001f\argon2-0.5.1\src\error.rs C:\Users\justi\.cargo\registry\src\index.crates.io-6f17d22bba15001f\argon2-0.5.1\src\params.rs C:\Users\justi\.cargo\registry\src\index.crates.io-6f17d22bba15001f\argon2-0.5.1\src\version.rs C:\Users\justi\.cargo\registry\src\index.crates.io-6f17d22bba15001f\argon2-0.5.1\src\../README.md
+
+C:\Users\justi\Desktop\Projects\Test\rust_test\target\debug\deps\argon2-c87b60e5b13a68e7.d: C:\Users\justi\.cargo\registry\src\index.crates.io-6f17d22bba15001f\argon2-0.5.1\src\lib.rs C:\Users\justi\.cargo\registry\src\index.crates.io-6f17d22bba15001f\argon2-0.5.1\src\algorithm.rs C:\Users\justi\.cargo\registry\src\index.crates.io-6f17d22bba15001f\argon2-0.5.1\src\blake2b_long.rs C:\Users\justi\.cargo\registry\src\index.crates.io-6f17d22bba15001f\argon2-0.5.1\src\block.rs C:\Users\justi\.cargo\registry\src\index.crates.io-6f17d22bba15001f\argon2-0.5.1\src\error.rs C:\Users\justi\.cargo\registry\src\index.crates.io-6f17d22bba15001f\argon2-0.5.1\src\params.rs C:\Users\justi\.cargo\registry\src\index.crates.io-6f17d22bba15001f\argon2-0.5.1\src\version.rs C:\Users\justi\.cargo\registry\src\index.crates.io-6f17d22bba15001f\argon2-0.5.1\src\../README.md
+
+C:\Users\justi\.cargo\registry\src\index.crates.io-6f17d22bba15001f\argon2-0.5.1\src\lib.rs:
+C:\Users\justi\.cargo\registry\src\index.crates.io-6f17d22bba15001f\argon2-0.5.1\src\algorithm.rs:
+C:\Users\justi\.cargo\registry\src\index.crates.io-6f17d22bba15001f\argon2-0.5.1\src\blake2b_long.rs:
+C:\Users\justi\.cargo\registry\src\index.crates.io-6f17d22bba15001f\argon2-0.5.1\src\block.rs:
+C:\Users\justi\.cargo\registry\src\index.crates.io-6f17d22bba15001f\argon2-0.5.1\src\error.rs:
+C:\Users\justi\.cargo\registry\src\index.crates.io-6f17d22bba15001f\argon2-0.5.1\src\params.rs:
+C:\Users\justi\.cargo\registry\src\index.crates.io-6f17d22bba15001f\argon2-0.5.1\src\version.rs:
+C:\Users\justi\.cargo\registry\src\index.crates.io-6f17d22bba15001f\argon2-0.5.1\src\../README.md:
---
+++ rust_test/target/debug/deps/rust_test-c0280e628733571c.d
@@ -0,0 +1,7 @@
+C:\Users\justi\Desktop\Projects\Test\rust_test\target\debug\deps\rust_test-c0280e628733571c.rmeta: src\main.rs src\cli.rs src\password_manager.rs
+
+C:\Users\justi\Desktop\Projects\Test\rust_test\target\debug\deps\rust_test-c0280e628733571c.d: src\main.rs src\cli.rs src\password_manager.rs
+
+src\main.rs:
+src\cli.rs:
+src\password_manager.rs:

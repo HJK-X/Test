2b1834a294e8de912df2a36095cc11648985766e
Justin <34551787+HJK-X@users.noreply.github.com>
2023-08-13 18:35:07 -0700

    dfgdfg

--- rust_test/src/cli.rs
+++ rust_test/src/cli.rs
@@ -1,15 +1,6 @@
-use clap::{Args, Parser, Subcommand};
+use clap::{Args, Parser};
 
 #[derive(Parser, Debug)]
-#[command(author, version, about, long_about = None)]
-#[command(propagate_version = true)]
-pub struct Cli {
-    #[command(subcommand)]
-    pub command: Commands,
-}
-
-
-#[derive(Subcommand, Debug)]
 pub enum Commands {
     /// Add a password
     Add(DefaultArgs),
--- rust_test/src/main.rs
+++ rust_test/src/main.rs
@@ -1,62 +1,43 @@
+use clap::Error as ClapError;
 use clap::Parser;
 use clap_repl::ClapEditor;
 use console::style;
-use rustyline::{DefaultEditor, Result};
 use rustyline::error::ReadlineError;
-use clap::Error as ClapError;
-use uuid::Uuid;
+use rustyline::{DefaultEditor, Result};
 use std::time::{Duration, SystemTime};
+use uuid::Uuid;
 
 mod cli;
 mod password_manager;
 
-fn main() -> Result<()> {
-    let mut rl = DefaultEditor::new()?;
+fn main() {
+    let mut rl = ClapEditor::<cli::Commands>::new();
 
     let password = rpassword::prompt_password("Your password: ").unwrap();
 
     loop {
-        let readline = rl.readline(">> ");
+        let Some(command) = rl.read_command() else {
+            continue;
+        };
 
-        match readline {
-            Ok(line) => {
-                let trimmed = line.trim();
-                
-                if trimmed == "quit" {
-                    println!("Goodbye!");
-                    ()
-                }
-                rl.add_history_entry(line.as_str());
-                
-                let cli_result = cli::Cli::parse_from(trimmed.split_whitespace());
-                if let Ok(cli) = cli_result {
-                    match &cli.command {
-                        cli::Commands::Add(args) => {
-                            handle_add_command(&args);
-                        }
-                        cli::Commands::Get(args) => {
-                            handle_get_command(&args);
-                        }
-                        cli::Commands::List(args) => {
-                            handle_list_command(&args);
-                        }
-                        cli::Commands::Update(args) => {
-                            handle_update_command(&args);
-                        }
-                        cli::Commands::Delete(args) => {
-                            handle_delete_command(&args);
-                        }
-                        cli::Commands::History(args) => {
-                            handle_history_command(&args);
-                        }
-                    }
-                }else {
-                    println!("Invalid command: {:?}", cli_result);
-                }
+        match command {
+            cli::Commands::Add(args) => {
+                handle_add_command(&args);
             }
-            Err(_) => {
-                println!("Error reading input.");
-                ()
+            cli::Commands::Get(args) => {
+                handle_get_command(&args);
+            }
+            cli::Commands::List(args) => {
+                handle_list_command(&args);
+            }
+            cli::Commands::Update(args) => {
+                handle_update_command(&args);
+            }
+            cli::Commands::Delete(args) => {
+                handle_delete_command(&args);
+            }
+            cli::Commands::History(args) => {
+                handle_history_command(&args);
             }
         }
     }
@@ -67,34 +48,45 @@ fn handle_add_command(args: &cli::DefaultArgs) {
     // You can access fields like args.website, args.username, args.password, etc.
     // ...
 
+    println!("Adding password for {:?}", args.website);
 }
 
 fn handle_get_command(args: &cli::DefaultArgs) {
     // Implement the logic to retrieve the password for a specific website using the provided arguments.
     // You can access fields like args.website, args.username, args.password, etc.
     // ...
+
+    println!("The password for {:?} is {:?}", args.website, args.password);
 }
 
 fn handle_list_command(args: &cli::DefaultArgs) {
     // Implement the logic to list all stored websites/services.
     // You can access fields like args.website, args.username, args.password, etc.
     // ...
+
+    println!("Listing passwords:");
 }
 
 fn handle_update_command(args: &cli::DefaultArgs) {
     // Implement the logic to update an existing password entry using the provided arguments.
     // You can access fields like args.website, args.username, args.password, etc.
     // ...
+    
+    println!("Updating password for {:?}", args.website);
 }
 
 fn handle_delete_command(args: &cli::DefaultArgs) {
     // Implement the logic to delete a password entry using the provided arguments.
     // You can access fields like args.website, args.username, args.password, etc.
     // ...
+    
+    println!("Deleting password for {:?}", args.website);
 }
 
 fn handle_history_command(args: &cli::DefaultArgs) {
     // Implement the logic to handle the "history" command if needed.
     // You can access fields like args.website, args.username, args.password, etc.
     // ...
+    
+    println!("Password history for {:?}:", args.website);
 }

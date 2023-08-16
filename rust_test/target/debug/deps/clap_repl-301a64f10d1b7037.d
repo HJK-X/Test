07e7b56da5a0a9a0b48289383e4bbe4a8658f571
Justin <34551787+HJK-X@users.noreply.github.com>
2023-08-16 01:00:17 -0700

    why do i have to mut self

---
+++ rust_test/target/debug/deps/clap_repl-301a64f10d1b7037.d
@@ -0,0 +1,7 @@
+C:\Users\justi\Desktop\Projects\Test\rust_test\target\debug\deps\clap_repl-301a64f10d1b7037.rmeta: C:\Users\justi\.cargo\registry\src\index.crates.io-6f17d22bba15001f\clap-repl-0.1.0\src/lib.rs
+
+C:\Users\justi\Desktop\Projects\Test\rust_test\target\debug\deps\libclap_repl-301a64f10d1b7037.rlib: C:\Users\justi\.cargo\registry\src\index.crates.io-6f17d22bba15001f\clap-repl-0.1.0\src/lib.rs
+
+C:\Users\justi\Desktop\Projects\Test\rust_test\target\debug\deps\clap_repl-301a64f10d1b7037.d: C:\Users\justi\.cargo\registry\src\index.crates.io-6f17d22bba15001f\clap-repl-0.1.0\src/lib.rs
+
+C:\Users\justi\.cargo\registry\src\index.crates.io-6f17d22bba15001f\clap-repl-0.1.0\src/lib.rs:

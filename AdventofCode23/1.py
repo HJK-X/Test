749567cf4e57fb50777a117353e6c6d1ec7b40c5
Justin <34551787+HJK-X@users.noreply.github.com>
2023-12-01 19:35:34 -0800

    aoc1

---
+++ AdventofCode23/1.py
@@ -0,0 +1,25 @@
+def isdigit(a, i):
+    l = ["one","two","three","four","five","six","seven","eight","nine"]
+    if a[i].isdigit():
+        return int(a[i])
+    
+    for j in range(len(l)):
+        if i+len(l[j]) <= len(a) and a[i:i+len(l[j])] == l[j]:
+            return j+1
+
+with open("1.txt", 'r') as f:
+    sm = 0
+    for a in f.readlines():
+        b = -1
+        l = -1
+        for i in range(len(a)):
+            d = isdigit(a, i)
+            if d != None:
+                if b == -1:
+                    b = d
+                l = d
+        
+        sm += b*10+l
+
+print(sm)
+
